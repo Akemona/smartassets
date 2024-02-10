85a679907c03e5ced73c7a988da6bec172e3a512
Akash <2557058+aksdevac@users.noreply.github.com>
2024-02-10 12:00:45 +0530

    verify support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,26 @@
+import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
+  sourcify: {
+    enabled: true,
+  },
+  networks: {
+    hardhat: {},
+    polygonMumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+    },
+    sepolia: {
+      url: "https://rpc.sepolia.org",
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY ?? "",
+      sepolia: process.env.ETHERSCAN_API_KEY ?? "",
+    },
+  },
   solidity: {
     version: "0.8.21",
     settings: {
